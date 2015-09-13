7e6b7957363535ee57dcadc816d3a08d4011a970
David Henry <david.henry@uswitch.com>
2015-09-13 01:03:18 +0100

    Added next game ticker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :only_one_login
+  helper_method :event
 
   protected
 
